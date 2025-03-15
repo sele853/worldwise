4a736f47150026ee8fcebabebc9273a57be7aad3
saladin <solehadinmoh@gmail.com>
2025-03-15 09:21:10 +0300

    fix the bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter,Navigate,Route,Routes} from "react-router-dom";
 import Product from "./pages/Product.jsx";
-import Homepage from "./pages/HomePage.jsx";
+import Homepage from "./pages/Homepage.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
 import Login from "./pages/Login.jsx";
